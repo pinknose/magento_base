7a1cd2f90ccc801ff400c4483df72f96ae987701
Martin Andersen <martin@pinknose.dk>
2013-04-27 20:11:39 +0200

    Modul tilføjet som fjerner valget af betalingsform og gør epay til standard

---
+++ app/code/local/Pinknose/Remove/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Pinknose_Remove_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ skin/frontend/default/default/js/removecheckout.js
@@ -0,0 +1,52 @@
+var Pinknose = Class.create(Checkout, {
+	initialize: function($super,accordion, urls){
+		$super(accordion, urls);
+		//New Code Addded
+		this.steps = ['login','billing', 'shipping', 'shipping_method', 'review'];
+	}
+});
+var PinknoseShippingMethod = Class.create(ShippingMethod , {
+	initialize: function($super,form, saveUrl){
+		$super(form, saveUrl);
+	},
+	nextStep: function(transport){
+        if (transport && transport.responseText){
+            try{
+                response = eval('(' + transport.responseText + ')');
+            }
+            catch (e) {
+                response = {};
+            }
+        }
+
+        if (response.error) {
+            alert(response.message);
+            return false;
+        }
+        checkout.setStepResponse(response);
+    }
+});
+var PinknoseReview = Class.create(Review,{
+	initialize: function($super,saveUrl, successUrl, agreementsForm){
+		$super(saveUrl, successUrl, agreementsForm);
+	},
+	save: function(){
+        if (checkout.loadWaiting!=false) return;
+        checkout.setLoadWaiting('review');
+        var params = 'payment[method]=epay_standard';
+        if (this.agreementsForm) {
+            params += '&'+Form.serialize(this.agreementsForm);
+        }
+        params.save = true;
+        var request = new Ajax.Request(
+            this.saveUrl,
+            {
+                method:'post',
+                parameters:params,
+                onComplete: this.onComplete,
+                onSuccess: this.onSave,
+                onFailure: checkout.ajaxFailure.bind(checkout)
+            }
+        );
+    }
+});
